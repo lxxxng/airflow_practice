58cd58f78a290617f49caac2cfed55e495b28408
= <wlxwulixing@gmail.com>
2025-04-20 22:06:05 +0800

    multiple task

--- dags/first_dag.py
+++ dags/first_dag.py
@@ -1,37 +1,45 @@
 from datetime import datetime, timedelta
+
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 
+
 default_args = {
-    "owner": "code2j",
-    "retries": 1,
-    "retry_delay": timedelta(minutes=2)
+    'owner': 'coder2j',
+    'retries': 5,
+    'retry_delay': timedelta(minutes=2)
 }
 
+
 with DAG(
-    dag_id='first_dag',
+    dag_id='our_first_dag_v5',
     default_args=default_args,
-    descirption='My first DAG',
-    schedule_interval='@daily',
-    start_date=datetime(2023, 10, 1),
+    description='This is our first dag that we write',
+    start_date=datetime(2021, 7, 29, 2),
+    schedule_interval='@daily'
 ) as dag:
-    # Task 1 (Upstream)
     task1 = BashOperator(
-        task_id='task1',
-        bash_command='echo "Task 1 completed"',
+        task_id='first_task',
+        bash_command="echo hello world, this is the first task!"
     )
 
-    # Task 2 (Downstream of task1)
     task2 = BashOperator(
-        task_id='task2',
-        bash_command='echo "Task 2 completed"',
+        task_id='second_task',
+        bash_command="echo hey, I am task2 and will be running after task1!"
     )
 
-    # Task 3 (Downstream of task2)
     task3 = BashOperator(
-        task_id='task3',
-        bash_command='echo "Task 3 completed"',
+        task_id='thrid_task',
+        bash_command="echo hey, I am task3 and will be running after task1 at the same time as task2!"
     )
 
-    # Set task dependencies (task1 -> task2 -> task3)
-    task1 >> task2 >> task3
+    # Task dependency method 1
+    # task1.set_downstream(task2)
+    # task1.set_downstream(task3)
+
+    # Task dependency method 2
+    # task1 >> task2
+    # task1 >> task3
+
+    # Task dependency method 3
+    task1 >> [task2, task3]
